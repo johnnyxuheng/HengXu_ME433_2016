6a10bf49e834fb4f0e0931f21e995266f9530deb
johnnyxuheng <hengxu2015@u.northwestern.edu>
2016-04-04 17:18:19 -0500

    Correct my understanding of questions

--- HW1/HW1.X/main.c
+++ HW1/HW1.X/main.c
@@ -64,13 +64,15 @@ int main() {
         _CP0_SET_COUNT(0);
         LATAbits.LATA4=0;
         while(_CP0_GET_COUNT()<12000){
+            ;
         }
         _CP0_SET_COUNT(0);
         LATAbits.LATA4=1;
         while(_CP0_GET_COUNT()<12000){
+            ;
         }
         while (PORTBbits.RB4==0){
-            LATAbits.LATA4=1;
+            ;
         }     
 	    // use _CP0_SET_COUNT(0) and _CP0_GET_COUNT() to test the PIC timing
 		// remember the core timer runs at half the CPU speed
